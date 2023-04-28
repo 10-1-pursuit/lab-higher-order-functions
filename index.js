377292e7ffab4ecdd375e8e9376c70a73cfb5fdb
Nat Vargas <nat485@gmail.com>
2023-04-28 14:01:02 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -14,9 +14,15 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element;
+
+    }
+
   }
+  return undefined;
 }
+
 
 /**
  * Returns an array of all elements in the array that cause the callback to return `true`. If the array is empty or no elements cause the callback to return `true`, then return an empty array.
@@ -35,10 +41,16 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element) === true) {
+      result.push(element)
+    }
+
   }
   return result;
 }
+
+
+
 
 /**
  * Returns an array where each element is transformed by the callback. If the array is empty, return an empty array.
@@ -57,7 +69,8 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element));
+
   }
   return result;
 }
@@ -77,7 +90,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array);
   }
 }
 
