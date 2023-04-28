10c4495c481a5d307b1445b058e3c4bc241a1173
Marcia  Foster <mfoster9tre@gmail.com>
2023-04-28 13:56:06 -0400

    completed lab

--- index.js
+++ index.js
@@ -14,8 +14,12 @@
  */
 function find(array, callback) {
   for (let element of array) {
-    // Write your code here.
+    if (callback(element)) {
+      return element;
+    }
+
   }
+  return undefined;
 }
 
 /**
@@ -35,7 +39,9 @@ function find(array, callback) {
 function filter(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    if (callback(element) === true) {
+      result.push(element)
+    }
   }
   return result;
 }
@@ -57,10 +63,12 @@ function filter(array, callback) {
 function map(array, callback) {
   const result = [];
   for (let element of array) {
-    // Write your code here.
+    result.push(callback(element));
   }
   return result;
 }
+
+
 
 /**
  * Does not return anything. Passes each element of the array into the callback along with the index and the array, in that order.
@@ -77,7 +85,7 @@ function map(array, callback) {
  */
 function forEach(array, callback) {
   for (let i = 0; i < array.length; i++) {
-    // Write your code here.
+    callback(array[i], i, array);
   }
 }
 
