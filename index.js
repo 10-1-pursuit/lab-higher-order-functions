f9c643fa12b07520983d5e25d56bff48e97b62a2
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-04-28 13:18:05 -0400

    Completed the first function

--- index.js
+++ index.js
@@ -14,6 +14,12 @@
  */
 function find(array, callback) {
   for (let element of array) {
+    if(callback(element)){
+      return element
+    }
+    // const callback = (element) => {
+    //   return element
+    // }
     // Write your code here.
   }
 }
